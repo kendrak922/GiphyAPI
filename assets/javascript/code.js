bcec9e05372a8287a45427c9769fb8c2a9ef7edf
Kendra Kyndberg <kendrak922@gmail.com>
2018-03-26 12:59:16 -0500

    add searches

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -7,6 +7,7 @@ const jQueryURL = "http://api.giphy.com/v1/gifs/search?q=" + search + key
 
 let topicsArray = ["cheeseburger", "coffee", "doughnut", "breakfast", "hot dog", "ice cream", "pizza", "apple", "pickle", "cookie", "salad", "banana"];
 
+// create buttons
 for (let i = 0; i < topicsArray.length; i++){
 let a = $("<button>");
 a.addClass("food");
@@ -14,5 +15,27 @@ a.attr("data-name", topicsArray[i])
 a.text(topicsArray[i]);
 $("#foodButton").append(a);
 }
-console.log("hello")
+
+// add new search
+$("#submit").click(function() {
+event.preventDefault();
+let newFood = $("#search").val().trim();
+if (newFood === undefined || newFood === '') {
+    newfood = 'code';
+  }
+let a = $("<button>")
+a.addClass("food");
+a.attr("data-name", newFood);
+a.text(newFood);
+$("#foodButton").append(a);
+// clearbox
+$("#search").val("");
 });
+
+
+});
+
+
+
+
+
