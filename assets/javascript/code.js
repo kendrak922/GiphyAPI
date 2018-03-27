49aa31b839b102a91effd72e6a9ae27b59c07516
Kendra Kyndberg <kendrak922@gmail.com>
2018-03-26 22:15:44 -0500

    added gifs to dom

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -1,11 +1,22 @@
 'use strict';
 $(document).ready(function(){
-let search = 0;
-const key = "api_key=LdQIqyjno3idEvd3Uv1emsa1PvCIt8l1"
-const jQueryURL = "http://api.giphy.com/v1/gifs/search?q=" + search + key
-
 
 let topicsArray = ["cheeseburger", "coffee", "doughnut", "breakfast", "hot dog", "ice cream", "pizza", "apple", "pickle", "cookie", "salad", "banana"];
+
+// call API
+function searchGif(search){
+const QueryURL = "https://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=LdQIqyjno3idEvd3Uv1emsa1PvCIt8l1&limit=10"
+$.ajax({
+    url: QueryURL,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
+    for(let i = 0; i < response.data.length; i++){
+    $(".gifs").append(`<img src="${response.data[i].images.fixed_height_small.url}"></img>`)
+}  
+console.log(response);
+});
+}
 
 // create buttons
 for (let i = 0; i < topicsArray.length; i++){
@@ -22,7 +33,7 @@ event.preventDefault();
 let newFood = $("#search").val().trim();
 if (newFood === undefined || newFood === '') {
     newfood = 'code';
-  }
+}
 let a = $("<button>")
 a.addClass("food");
 a.attr("data-name", newFood);
@@ -30,12 +41,19 @@ a.text(newFood);
 $("#foodButton").append(a);
 // clearbox
 $("#search").val("");
+searchGif(newFood)
 });
 
+$("button").click(function(){
+let buttonFood = $(this).val() 
+console.log(buttonFood)
+    searchGif(buttonFood)
+}
+
+)
+
+
 
 });
-
-
-
 
 
